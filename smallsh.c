7b803df0096194c4e18c37d414c5b654cb814875
Colin Francis <francico@os1.engr.oregonstate.edu>
2022-01-24 13:07:02 -0800

    Updated command struct to have attributes for input and output redirection. Code implemented in parse function to create path name of bin executable from user input

--- smallsh.c
+++ smallsh.c
@@ -11,24 +11,53 @@
 struct command {
 	char* pathName;
 	char** argv;
+	bool inputRedirect;
+	char* newInput;
+	bool outputRedirect;
+	char* newOutput;
 };
 
-struct command* getCommandLineInput(void) {
+char* getCommandLineInput(void) {
 	char* userInput = NULL;
+	char* inputBuffer = NULL;
 	size_t length;
 
 	printf(": ");
-	getline(&userInput, &length, stdin);
-	*(userInput + strlen(userInput) - 1) = '\0';
+	getline(&inputBuffer, &length, stdin);
+	*(inputBuffer + strlen(inputBuffer) - 1) = '\0';
 
-	printf("%s\n", userInput);
+	userInput = (char*)malloc((strlen(inputBuffer) + 1) * sizeof(char));
+	strcpy(userInput, inputBuffer);
+
+	return userInput;
+}
+
+struct command* parseUserInput(char* userInput) {
+	char* token = NULL;
+	char* savePtr = NULL;
+	struct command* command = (struct command*)malloc(sizeof(struct command));
+
+	token = strtok_r(userInput, " ", &savePtr);
+	if (!token || (strcmp(token, "#") == 0)) {
+		return NULL;
+	}
+	command->pathName = (char*)malloc((strlen(token) + strlen("/bin/") + 1) * sizeof(char));
+	strcpy(command->pathName, "/bin/");
+	strcat(command->pathName, token);
+
+	return command;
 }
 
 int main(void) {
-	struct command* userInput = NULL;
+	char* userInput = NULL;
+	struct command* command = NULL;
 
 	while (true) {
 		userInput = getCommandLineInput();
+		command = parseUserInput(userInput);
+		if (!command) {
+			continue;
+		}
 	}
 
 	return EXIT_SUCCESS;
