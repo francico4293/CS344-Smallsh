4414a6391d97ce3a5c42dcc852eb32c606ffadfe
Colin Francis <francico@os1.engr.oregonstate.edu>
2022-01-24 14:37:08 -0800

    Continuing to work on parser function for user input to command line. I have implemented conditional statements to look for input redirection, output redirection, and a call to make the process a background process. In the process of completing code that creates argv array

--- smallsh.c
+++ smallsh.c
@@ -11,10 +11,11 @@
 struct command {
 	char* pathName;
 	char** argv;
-	bool inputRedirect;
+	bool inputRedirect;  // <
 	char* newInput;
-	bool outputRedirect;
+	bool outputRedirect;  // >
 	char* newOutput;
+	bool backgroundProcess;  // &
 };
 
 char* getCommandLineInput(void) {
@@ -32,10 +33,46 @@ char* getCommandLineInput(void) {
 	return userInput;
 }
 
+void initializeCommandStruct(struct command* command, int numArgs) {
+	command->inputRedirect = false;
+	command->newInput = NULL;
+
+	command->outputRedirect = false;
+	command->newOutput = NULL;
+
+	command->backgroundProcess = false;
+
+	command->argv = (char**)malloc(numArgs * sizeof(char*));
+	for (int index = 0; index < numArgs; index++) {
+		command->argv[index] = NULL;
+	}
+}
+
+char** appendArg(char* arg, char* argv[], int numArgs, int argvIndex) {
+	int index = 0;
+	char** newArgv = (char**)malloc((numArgs + 1) * sizeof(char*));
+
+	argv[argvIndex] = (char*)malloc((strlen(arg) + 1) * sizeof(char));
+	strcpy(argv[argvIndex], arg);
+
+	while (index <= numArgs) {
+		newArgv[index] = argv[index];
+		index++;
+	}
+	newArgv[index] = NULL;
+
+	free(argv);
+	return newArgv;
+}
+
 struct command* parseUserInput(char* userInput) {
+	int numArgs = 2;
+	int argvIndex = 0;
 	char* token = NULL;
 	char* savePtr = NULL;
 	struct command* command = (struct command*)malloc(sizeof(struct command));
+
+	initializeCommandStruct(command, numArgs);
 
 	token = strtok_r(userInput, " ", &savePtr);
 	if (!token || (strcmp(token, "#") == 0)) {
@@ -44,6 +81,33 @@ struct command* parseUserInput(char* userInput) {
 	command->pathName = (char*)malloc((strlen(token) + strlen("/bin/") + 1) * sizeof(char));
 	strcpy(command->pathName, "/bin/");
 	strcat(command->pathName, token);
+
+	command->argv[argvIndex] = command->pathName;
+	argvIndex++;
+
+	token = strtok_r(NULL, " ", &savePtr);
+	while (token) {
+		if (strcmp(token, "<") == 0) {
+			command->inputRedirect = true;
+		}
+		else if (strcmp(token, ">") == 0) {
+			command->outputRedirect = true;
+		}
+		else if (strcmp(token, "&") == 0) {
+			command->backgroundProcess = true;
+		}
+
+		command->argv = appendArg(token, command->argv, numArgs, argvIndex);
+		argvIndex++;
+		numArgs++;
+
+		token = strtok_r(NULL, " ", &savePtr);
+	}
+
+	for (int index = 0; index < numArgs; index++) {
+		printf("%s ", command->argv[index]);
+	}
+	printf("\n");
 
 	return command;
 }
